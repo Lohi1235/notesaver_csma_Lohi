4c32c86803b134390c2390a543d7964f287dd0fb
Lohi1235 <lohitha.jalla24@sasi.ac.in>
2026-07-14 12:00:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use("/", noteRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(mongodb+srv:lohithajalla24_db_user:lohitha23@cluster0.9eymuus.mongodb.net/lohitha)
 .then(() => {
     console.log("MongoDB Connected Successfully");
 })
